1f9aa3e2b41f0762bcd0ff03e6f31a8f3cb9f316
Perttu <perttu@gmail.com>
2015-03-27 20:57:05 +0200

    Tutorial completed
    Working save + retrieve; changed .asObject to .asArray to enable
    sorting on client side; added index to weekdays for correct sort

--- app.js
+++ app.js
@@ -11,5 +11,116 @@ angular.module('scheduleApp', ['firebase'])
 
   var fb = $firebase(ref);
 
+  // sync as object
+  var list = fb.$asArray();
+
+  // three way data binding
+  // syncObject.$bindTo($scope, 'days');
+$scope.days = list;
+
+  // function to set the default data
+  $scope.reset = function() {
+
+    fb.$set({
+      monday: {
+        index: 0,
+        name: 'Monday',
+        slots: {
+          "0900": {
+            time: '9:00am',
+            booked: false
+          },
+          "1100": {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      },
+      tuesday: {
+        index: 1,
+        name: 'Tuesday',
+        slots: {
+          "0900": {
+            time: '9:00am',
+            booked: false
+          },
+          "1100": {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      },
+      wednesday: {
+        index: 2,
+        name: 'Wednesday',
+        slots: {
+          "0900": {
+            time: '9:00am',
+            booked: false
+          },
+          "1100": {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      },
+      thursday: {
+        index: 3,
+        name: 'Thursday',
+        slots: {
+          "0900": {
+            time: '9:00am',
+            booked: false
+          },
+          "1100": {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      },
+      friday: {
+        index: 4,
+        name: 'Friday',
+        slots: {
+          "0900": {
+            time: '9:00am',
+            booked: false
+          },
+          "1100": {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      },
+      saturday: {
+        index: 5,
+        name: 'Saturday',
+        slots: {
+          "0900": {
+            time: '9:00am',
+            booked: false
+          },
+          "1100": {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      },
+      sunday: {
+        index: 6,
+        name: 'Sunday',
+        slots: {
+          "0900": {
+            time: '9:00am',
+            booked: false
+          },
+          "1100": {
+            time: '11:00am',
+            booked: false
+          }
+        }
+      }
+    });
+  };
 
 });
